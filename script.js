1f3a692e2c8502d57aca1df480284a97e5137ad7
Ariel Cameron <ariel.cameron@gmail.com>
2024-07-14 18:36:23 -0700

    Added functions to randomize color of grid squares

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function createGrid(rows, columns) {
         console.log(`Created grid item ${i + 1}`); // Log each grid item creation
 
         gridItem.addEventListener("mouseover", () => {
-            gridItem.style.backgroundColor = "yellow";
+            gridItem.style.backgroundColor = randomHexColor();
         });
     }
 
@@ -52,6 +52,28 @@ function showForm() {
 // Function to hide the popup form
 function hideForm() {
     document.getElementById("myForm").style.display = "none";
+}
+
+
+function randomInteger(max) {
+    return Math.floor(Math.floor() * (max + 1));
+}
+
+function randomRgbColor() {
+    let r = randomInteger(255);
+    let g = randomInteger(255);
+    let b = randomInteger(255);
+    return [r,g,b];
+}
+
+function randomHexColor() {
+    let [r,g,b] = randomRgbColor();
+
+    let hr = r.toString(16).padStart(2, '0');
+    let hg = g.toString(16).padStart(2, '0');
+    let hb = b.toString(16).padStart(2, '0');
+
+    return "#" + hr + hg + hb;
 }
 
 // Initial grid creation
